52ad4f6cde54e32b2e4ef8526bdbb9d0e76afdb4
thnf <74548231+thnf@users.noreply.github.com>
2022-12-23 07:59:12 +0800

    save

--- earthnfc-badge.js
+++ earthnfc-badge.js
@@ -10,9 +10,7 @@ var _hmt = _hmt || [];
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
- charset="UTF-8" id="LA_COLLECT" src="//sdk.51.la/js-sdk-pro.min.js">
-LA.init({id:"JuwJmSVQNTdeACxY",ck:"JuwJmSVQNTdeACxY",autoTrack:true,hashMode:true})
-
+!function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"JuwJmSVQNTdeACxY",ck:"JuwJmSVQNTdeACxY",autoTrack:true,hashMode:true});
   // Suppress in ReplView
   if (window.location.hostname.split('.')[1] === 'id') return;
 
